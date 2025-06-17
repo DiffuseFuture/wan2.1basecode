f0d40273c5af8809af7f5792145a718b239d90bb
jiangxinxi95 <14657301+jiangxinxi95@user.noreply.gitee.com>
2025-06-17 19:26:36 +0800

    train14b_stage_1_1_accumulation.sh

--- VideoX-Fun/scripts/wan2.1_fun/train14b_stage_1_1_accumulation.sh
+++ VideoX-Fun/scripts/wan2.1_fun/train14b_stage_1_1_accumulation.sh
@@ -49,4 +49,5 @@ accelerate launch --num_machines 2 --num_processes 16 --machine_rank ${RANK} --m
   --enable_bucket \
   --uniform_sampling \
   --train_mode="inpaint" \
-  --trainable_modules "."
+  --trainable_modules "." \
+  --low_vram
